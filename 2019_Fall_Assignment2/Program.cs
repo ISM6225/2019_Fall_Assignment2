f5496ca8b4e3a7c413834928539284585c7de3d2
Rohan <rohanro74@gmail.com>
2019-10-05 16:51:58 -0400

    complered SortedSquares function with O(N) time complexity.

--- 2019_Fall_Assignment2/Program.cs
+++ 2019_Fall_Assignment2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _2019_Fall_Assignment2
 {
@@ -41,7 +42,8 @@ namespace _2019_Fall_Assignment2
             Console.Write("\n");
 
             string s = "abca";
-            if(ValidPalindrome(s)) {
+            if (ValidPalindrome(s))
+            {
                 Console.WriteLine("The given string \"{0}\" can be made PALINDROME", s);
             }
             else
@@ -52,7 +54,7 @@ namespace _2019_Fall_Assignment2
 
         public static void DisplayArray(int[] a)
         {
-            foreach(int n in a)
+            foreach (int n in a)
             {
                 Console.Write(n + " ");
             }
@@ -60,9 +62,9 @@ namespace _2019_Fall_Assignment2
 
         public static void Display2DArray(int[,] a)
         {
-            for(int i=0;i<a.GetLength(0);i++)
+            for (int i = 0; i < a.GetLength(0); i++)
             {
-                for(int j=0;j<a.GetLength(1);j++)
+                for (int j = 0; j < a.GetLength(1); j++)
                 {
                     Console.Write(a[i, j] + "\t");
                 }
@@ -81,21 +83,21 @@ namespace _2019_Fall_Assignment2
                 {
                     mid = start + (end - start) / 2;
 
-                    if(nums[mid] == target)
+                    if (nums[mid] == target)
                     {
                         return mid;
                     }
 
                     if (nums[mid] < target)
                     {
-                         start = mid + 1;
+                        start = mid + 1;
                     }
                     else
                     {
                         end = mid - 1;
                     }
 
-                  
+
                 }
 
                 return -1;
@@ -113,7 +115,22 @@ namespace _2019_Fall_Assignment2
         {
             try
             {
-                // Write your code here
+                // Using Dictionaries
+                Dictionary<int, int> n1 = new Dictionary<int, int>();
+                Dictionary<int, int> n2 = new Dictionary<int, int>();
+
+                int[] output = new int[nums1.Length];
+
+                for (int i = 0; i < nums1.Length; i++)
+                {
+                    n1.Add(i, nums1[i]);
+                }
+                for (int j = 0; j < nums2.Length; j++)
+                {
+                    n2.Add(j, nums2[j]);
+                }
+
+
             }
             catch
             {
@@ -183,7 +200,15 @@ namespace _2019_Fall_Assignment2
         {
             try
             {
-                // Write your code here
+                for (int i = 0; i < A.Length; i++)
+                {
+                    A[i] *= A[i];
+                }
+                //to sort an array with O(N) we will use radix sort 
+                radixsort(A, A.Length);
+
+                return A;
+
             }
             catch
             {
@@ -205,6 +230,61 @@ namespace _2019_Fall_Assignment2
             }
 
             return false;
+        }
+
+        public static int getMax(int[] arr, int n)
+        {
+            int mx = arr[0];
+            for (int i = 1; i < n; i++)
+                if (arr[i] > mx)
+                    mx = arr[i];
+            return mx;
+        }
+
+        // A function to do counting sort of arr[] according to  
+        // the digit represented by exp.  
+        public static void countSort(int[] arr, int n, int exp)
+        {
+            int[] output = new int[n]; // output array  
+            int i;
+            int[] count = new int[10];
+
+            //initializing all elements of count to 0 
+            for (i = 0; i < 10; i++)
+                count[i] = 0;
+
+            // Store count of occurrences in count[]  
+            for (i = 0; i < n; i++)
+                count[(arr[i] / exp) % 10]++;
+
+            // Change count[i] so that count[i] now contains actual  
+            //  position of this digit in output[]  
+            for (i = 1; i < 10; i++)
+                count[i] += count[i - 1];
+
+            // Build the output array  
+            for (i = n - 1; i >= 0; i--)
+            {
+                output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+                count[(arr[i] / exp) % 10]--;
+            }
+
+            // Copy the output array to arr[], so that arr[] now  
+            // contains sorted numbers according to current digit  
+            for (i = 0; i < n; i++)
+                arr[i] = output[i];
+        }
+
+        public static void radixsort(int[] arr, int n)
+        {
+            // Find the maximum number to know number of digits  
+            int m = getMax(arr, n);
+
+            // Do counting sort for every digit. Note that instead  
+            // of passing digit number, exp is passed. exp is 10^i  
+            // where i is current digit number  
+            for (int exp = 1; m / exp > 0; exp *= 10)
+                countSort(arr, n, exp);
         }
     }
 }
