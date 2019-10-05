8f943f1aa9e2af432e884c7b76cde939585245aa
Rohan <rohanro74@gmail.com>
2019-10-04 22:13:41 -0400

    updated SearchInsert using binary search. need to work on return the index of the target if it is found in the array

--- 2019_Fall_Assignment2/Program.cs
+++ 2019_Fall_Assignment2/Program.cs
@@ -6,7 +6,7 @@ namespace _2019_Fall_Assignment2
     {
         public static void Main(string[] args)
         {
-            int target = 5;
+            int target = 7;
             int[] nums = { 1, 3, 5, 6 };
             Console.WriteLine("Position to insert {0} is = {1}\n", target, SearchInsert(nums, target));
 
@@ -74,7 +74,32 @@ namespace _2019_Fall_Assignment2
         {
             try
             {
-                // Write your code here
+                //use binary search
+                int start = 0, end = nums.Length;
+                int mid;
+                while (start <= end)
+                {
+                    mid = start + (end - start) / 2;
+
+                    if(nums[mid] == target)
+                    {
+                        return mid;
+                    }
+
+                    if (nums[mid] < target)
+                    {
+                         start = mid + 1;
+                    }
+                    else
+                    {
+                        end = mid - 1;
+                    }
+
+                  
+                }
+
+                return -1;
+
             }
             catch
             {
